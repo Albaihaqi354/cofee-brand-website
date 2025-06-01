40951ba16582dd9a5ba430c847dca3c6bcee0be5
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-06-01 15:29:02 +0700

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Coffee Web',
+  title: 'Coffee Web ',
   description: 'Beone MoonWork',
   generator: 'Git',
 }
